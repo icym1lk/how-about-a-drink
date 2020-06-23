85a4736fbd7bc63c6a6982a86df52a8fd6a425d5
Jason Hanrahan <jasonhanr@gmail.com>
2020-06-23 13:04:04 -0400

    Add select list to /results search bar. Only Letter route implemented

--- app.py
+++ app.py
@@ -16,12 +16,13 @@ def homepage():
 
     return render_template("index.html")
 
-@app.route("/results")
+@app.route("/results", methods=["POST"])
 def search_results():
     """Render search results."""
-    query = request.args['query']
+    query_type = request.form['query_type']
+    query = request.form['query']
     res = requests.get(f'https://www.thecocktaildb.com/api/json/v1/1/search.php?',
-                                params = {'f': query})
+                                params = {query_type: query})
     data = res.json()
     # import pdb
     # pdb.set_trace()
