677b62305fbb0d6130c290db55ef7c01e0f30a4f
Jason Hanrahan <jasonhanr@gmail.com>
2020-06-25 09:42:52 -0400

    Add drinkID to data attributes and retrieve with click using jQuery

--- static/app.js
+++ static/app.js
@@ -1,3 +1,9 @@
+const $drinkImage = $('#drink-image');
+
 $('#query_type').change(function() {
 	$('#query').attr('placeholder', $(this).find(':selected').data('name'));
+});
+
+$drinkImage.on('click', function(e) {
+	let $drinkID = $(this).data('drink-id');
 });
