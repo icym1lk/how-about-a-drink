e1273d4f3d48f8c7b275992150654f7c39a880de
Jason Hanrahan <jasonhanr@gmail.com>
2020-06-25 09:52:03 -0400

    Add onClick API call functionality on img of search results to get drink details

--- static/app.js
+++ static/app.js
@@ -6,4 +6,22 @@ $('#query_type').change(function() {
 
 $drinkImage.on('click', function(e) {
 	let $drinkID = $(this).data('drink-id');
+	callAPI($drinkID);
 });
+
+async function callAPI(id) {
+	const res = await axios({
+		method: 'get',
+		url: `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+	})
+		.then((res) => {
+			console.log('success');
+			console.log(res);
+			// handleResponseSuccess(res.data);
+		})
+		.catch((err) => {
+			console.log('fail');
+			console.log(err.response);
+			// handleResponseFail(err.response.data);
+		});
+}
