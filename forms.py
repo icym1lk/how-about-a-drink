198723602def4942521477d3489d3c8735d7a7a1
Jason Hanrahan <jasonhanr@gmail.com>
2020-07-06 10:53:37 -0400

    Add User models and forms. Install email_validator

--- forms.py
+++ forms.py
@@ -1,9 +1,31 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField
-from wtforms.validators import InputRequired, Optional, URL, NumberRange
+from wtforms import StringField, PasswordField, SelectField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email, Lenght, InputRequired
+import email_validator
 
 class SearchAPIForm(FlaskForm):
     """Form for searching API."""
 
     query = StringField("Query", validators=[InputRequired()])
     query_type = SelectField("Query Type", choices=[("s", "Cocktail"), ("i", "Ingredient"), ("f", "Letter / No.")])
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = EmailField('E-mail', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+class UserEditForm(FlaskForm):
+    """Form for editing users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = EmailField('E-mail', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
--- models.py
+++ models.py
@@ -1,8 +1,103 @@
 # import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
 # intialize a variable for our DB by running SQLAlchemy. db is standard name
 db = SQLAlchemy()
+
+class Favorite(db.Model):
+    """Mapping user favorites."""
+
+    __tablename__ = 'favorites' 
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete='cascade')
+    )
+
+    drink_id = db.Column(
+        db.Integer,
+        unique=True
+    )
+
+class User(db.Model):
+    """User in the system."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    email = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    username = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    # favorites = db.relationship(
+    #     'Drink',
+    #     secondary="favorites"
+    # )
+
+    def __repr__(self):
+        return f"<User #{self.id}: {self.username}, {self.email}>"
+
+    @classmethod
+    def signup(cls, username, email, password):
+        """Sign up user.
+
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            email=email,
+            password=hashed_pwd
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If can't find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
 
 # associate Flask app with our DB
 # don't want to connect to a db every single time you run your models file
@@ -11,3 +106,4 @@ def connect_db(app):
     """Connect to db"""
     db.app = app
     db.init_app(app)
+
